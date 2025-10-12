9d2010639f1124c32277abb5ec0eed8eefb7df7c
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-10-12 07:25:56 +0200

    Add time state

--- src/components/Stoper/Stoper.js
+++ src/components/Stoper/Stoper.js
@@ -1,10 +1,21 @@
 import styles from './Stoper.module.scss';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 const Stoper = () => {
+  const [time, setTime] = useState({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    miliseconds: 0
+  });
+
   return(
     <>
-      <div className={styles.stoper}>00:00:00.000</div>
+      <div className={styles.stoper}>{time.hours < 10 ? '0' + time.hours : time.hours}:
+        {time.minutes < 10 ? '0' + time.minutes : time.minutes}:
+        {time.seconds < 10 ? '0' + time.seconds : time.seconds}.
+        {time.miliseconds < 100 ? (time.miliseconds < 10 ? '00' + time.miliseconds : '0' + time.miliseconds) : time.miliseconds}</div>
       <section className={styles.buttonsSection}>
         <Button>Start</Button>
         <Button>Stop</Button>
