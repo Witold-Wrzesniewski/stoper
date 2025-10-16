464e3a976b5557e57ae9171cac25e1b12acad22c
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-10-16 21:05:12 +0200

    Change Stoper

--- src/components/Stoper/Stoper.js
+++ src/components/Stoper/Stoper.js
@@ -1,54 +1,54 @@
 import styles from './Stoper.module.scss';
 import Button from '../Button/Button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Stoper = () => {
-  const time = new Date();
-  const [startTime, setStartTime] = useState(time.getTime());
-  const [currentTime, setCurrentTime] = useState({
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-    milliseconds: 0
-  });
-  /*const timer = setInterval(() => {
-    newTime(time.getTime() - startTime);
-  }, 1000);*/
+  const [startTime, setStartTime] = useState(new Date().getTime());
+  const [currentTime, setCurrentTime] = useState(new Date().getTime());
 
-  const newTime = (increment) => {
-    let [milliseconds, seconds, minutes, hours] = [currentTime.milliseconds, currentTime.seconds, currentTime.minutes, currentTime.hours];
+  //const [timer, setTimer] = useState(setInterval(() => setCurrentTime(new Date().getTime()), 1000));
+  const [timer, setTimer] = useState(null);
 
-    milliseconds += increment;
+  useEffect(() => {
+    setTimer(setInterval(() => setCurrentTime(new Date().getTime()), 1000));
+    return () => {
+      if(timer) clearInterval(timer);
+    };
+  }, []);
+  const time = currentTime - startTime;
+
+  const formatedTime = (time) => {
+    let milliseconds = time, seconds = 0, minutes = 0, hours = 0;
+
     if (milliseconds > 999) {
-      seconds += Math.floor(milliseconds / 1000);
+      seconds = Math.floor(milliseconds / 1000);
       milliseconds = milliseconds % 1000;
     }
     if (seconds > 59) {
-      minutes += Math.floor(seconds / 60);
+      minutes = Math.floor(seconds / 60);
       seconds = seconds % 60;
     }
     if (minutes > 59) {
-      hours += Math.floor(minutes / 60);
+      hours = Math.floor(minutes / 60);
       minutes = minutes % 60;
     }
 
-    setCurrentTime({
+    return {
       hours: hours,
       minutes: minutes,
       seconds: seconds,
       milliseconds: milliseconds
-    });
+    };
   };
 
   return(
     <>
-      <div className={styles.stoper}>{currentTime.hours < 10 ? '0' + currentTime.hours : currentTime.hours}:
-        {currentTime.minutes < 10 ? '0' + currentTime.minutes : currentTime.minutes}:
-        {currentTime.seconds < 10 ? '0' + currentTime.seconds : currentTime.seconds}.
-        {currentTime.milliseconds < 100 ? (currentTime.milliseconds < 10 ? '00' + currentTime.milliseconds : '0' + currentTime.milliseconds) : currentTime.milliseconds}</div>
-        <div className={styles.stoper}>{time.getTime()}</div>
+      <div className={styles.stoper}>{formatedTime(time).hours < 10 ? '0' + formatedTime(time).hours : formatedTime(time).hours}:
+        {formatedTime(time).minutes < 10 ? '0' + formatedTime(time).minutes : formatedTime(time).minutes}:
+        {formatedTime(time).seconds < 10 ? '0' + formatedTime(time).seconds : formatedTime(time).seconds}.
+        {formatedTime(time).milliseconds < 100 ? (formatedTime(time).milliseconds < 10 ? '00' + formatedTime(time).milliseconds : '0' + formatedTime(time).milliseconds) : formatedTime(time).milliseconds}</div>
       <section className={styles.buttonsSection}>
-        <Button action={() => newTime(1000)}>Start</Button>
+        <Button>Start</Button>
         <Button>Stop</Button>
         <Button>Reset</Button>
       </section>
